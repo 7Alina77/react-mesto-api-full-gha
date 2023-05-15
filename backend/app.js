653291db0fb7ded241c4d6e61cb62d27242f513a
Alina Potapchuk <alinapotapchuk7@gmail.com>
2023-05-15 20:46:25 +0300

    fix of cors

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,10 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const routes = require('./routes/index');
 const { handleErrors } = require('./errors/handleErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -18,7 +18,16 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors);
+
+const corsAllowed = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsAllowed));
 app.use(requestLogger);
 app.use(routes);
 app.use(errorLogger);
