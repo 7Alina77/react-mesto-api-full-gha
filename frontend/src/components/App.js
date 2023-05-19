ac994e84809972ccd9f1e17eb5aee4ed0a1a472c
Alina Potapchuk <alinapotapchuk7@gmail.com>
2023-05-20 01:43:10 +0300

    fix of likes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -105,7 +105,7 @@ function App() {
   
   function handleCardLike(card) {
     // Снова проверяем, есть ли уже лайк на этой карточке
-    const isLiked = card.likes.some(i => i === currentUser._id);
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
     // Отправляем запрос в API и получаем обновлённые данные карточки
     api.changeLikeCardStatus(card._id, !isLiked)
       .then((newCard) => {
