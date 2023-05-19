27df3f905caa0071428646eda8df90d1c52d9b44
Alina Potapchuk <alinapotapchuk7@gmail.com>
2023-05-20 01:17:31 +0300

    fix of i._id in cardjs appjs

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -104,13 +104,11 @@ function App() {
   }
   
   function handleCardLike(card) {
-    console.log(card);
     // Снова проверяем, есть ли уже лайк на этой карточке
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const isLiked = card.likes.some(i => i === currentUser._id);
     // Отправляем запрос в API и получаем обновлённые данные карточки
     api.changeLikeCardStatus(card._id, !isLiked)
       .then((newCard) => {
-        console.log(newCard);
         setCards((state) => state.map((c) => c._id === card._id ? newCard : c))
       })
       .catch((err) => {
